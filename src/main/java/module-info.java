e2baa62d7214a66c2e85e7d2921927604c1a874c
jbiancaniello <76523489+jbiancaniello@users.noreply.github.com>
2022-07-07 11:54:28 -0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.jbiancaniello.ideapicker {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+        requires org.controlsfx.controls;
+                        requires org.kordamp.bootstrapfx.core;
+            
+    opens com.jbiancaniello.ideapicker to javafx.fxml;
+    exports com.jbiancaniello.ideapicker;
+}
